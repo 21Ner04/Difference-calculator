18a218459a016f19823e540fb01facc2fdef2381
21Ner04 <ttobievna@yandex.ru>
2023-11-22 19:34:38 +0200

    update2step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,5 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
-
+  .arguments('<filePath1> <filePath2> ')
 program.parse();
