f784eb947c59e0b6df13e07256184b72c56b42cc
21Ner04 <ttobievna@yandex.ru>
2023-11-26 16:50:49 +0200

    3step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { program } from 'commander';
 
-const program = new Command();
+import  gendiff  from '../src/index.js';
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filePath1> <filePath2> ')
+  .action((filePath1, filePath2) => {
+    console.log(gendiff(filePath1, filePath2, program.opts().format));
+  });
+
 program.parse();
